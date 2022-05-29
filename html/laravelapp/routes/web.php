61cb6dbd2ec2a34e652ba7f2d52ed72309cc574d
Yuzunoha <studysteadystandby@gmail.com>
2022-05-30 01:16:34 +0900

    Route::get('/', fn () => redirect('/register'));

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn () => redirect('/dashboard'));
+Route::get('/', fn () => redirect('/register'));
 Route::get('markdown', [MarkdownController::class, 'index']);
 Route::post('markdown/store', [MarkdownController::class, 'store']);
 Route::get('markdown/setsample', [MarkdownController::class, 'setsample']);
