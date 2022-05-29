cb3ce84eff98e55dc5ffa1a739957cabd3312aa2
Yuzunoha <studysteadystandby@gmail.com>
2022-05-30 01:12:00 +0900

    livewireの件

--- html/laravelapp/routes/web.php
+++ html/laravelapp/routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', fn () => redirect('markdown'));
 Route::get('markdown', [MarkdownController::class, 'index']);
 Route::post('markdown/store', [MarkdownController::class, 'store']);
 Route::get('markdown/setsample', [MarkdownController::class, 'setsample']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
