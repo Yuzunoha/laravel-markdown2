e0c8f303df4dd5c2406fb7c049fc4770171fc73a
Yuzunoha <studysteadystandby@gmail.com>
2022-05-31 00:27:34 +0900

    整理した

--- html/laravelapp/routes/api.php
+++ html/laravelapp/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\GoController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('me', [UserController::class, 'me']);
+    Route::any('go', [GoController::class, 'index']);
+    Route::any('go/test', [GoController::class, 'test']);
 });
-Route::any('go', [GoController::class, 'index']);
-Route::any('go/test', [GoController::class, 'test']);
